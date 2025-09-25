8104e6173bf6663d1fdd75e20e4ac6dd02480d7c
tar7nic <154546879+tar7nic@users.noreply.github.com>
2025-09-25 23:00:11 +0530

    Add files via upload

---
+++ SQL/learning schema_customer table.sql
@@ -0,0 +1,14 @@
+SELECT * FROM learning.customers;
+
+-- INSERT INTO learning.customers
+-- values
+-- (101, 'John','Doe')
+
+-- INSERT INTO learning.customers
+-- values
+-- (102, 'Sarah','Jones'),
+-- (103, 'Richard','Parker')
+
+drop table customers;
+
+drop schema learning;
